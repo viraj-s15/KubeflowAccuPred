cfe0d1ee37c96c9359d602155d8a24e8bab614cf
Viraj Shah viraj-s15 <viraj.v.shah03@gmail.com>
2023-11-28 18:40:59 +0530

    feat: fixed error handling

--- data_preprocessing/data_preprocessing.py
+++ data_preprocessing/data_preprocessing.py
@@ -1,33 +1,39 @@
 def data_preprocessing() -> None:
     import pandas as pd
-    import numpy as np 
-    import logging 
+    import numpy as np
+    import logging
     from sklearn.preprocessing import LabelEncoder
-    logging.basicConfig(level = logging.INFO)
+    
+    logging.basicConfig(level=logging.INFO)
     print('#' * 100)
     logging.info("Starting Data Preprocessing Component")
-    filename="data/data.csv"
-    df = pd.read_csv(filename)
-    logging.info("File has been loaded")
-    missing = sum(df.isna().sum())
-    logging.info(f"Missing elements: {missing}")
     
-    cols = df.columns
-    object_cols = []
-    for i in cols:
-        col = df[i]
-        if col.dtype == "object":
-            object_cols.append(i)
+    try:
+        filename = "data/data.csv"
+        df = pd.read_csv(filename)
+        logging.info("File has been loaded")
 
-    logging.info(f"The columns which contain objects are: {', '.join(object_cols)}")
-    logging.info("These columns will be encoded")    
-    le = LabelEncoder()
-    logging.info("Encoding.......")
-    for col in object_cols:
-        df[col] = le.fit_transform(df[col])
-    logging.info("Encoding complete")
-    df.to_csv("data/processed_data.csv",index=False)
-    logging.info("Preprocessed csv file has been saved into data/processed_data.csv")
+        missing = sum(df.isna().sum())
+        logging.info(f"Missing elements: {missing}")
+
+        cols = df.columns
+        object_cols = [col for col in cols if df[col].dtype == "object"]
+
+        if object_cols:
+            logging.info(f"The columns which contain objects are: {', '.join(object_cols)}")
+            logging.info("These columns will be encoded")
+            le = LabelEncoder()
+            logging.info("Encoding.......")
+            for col in object_cols:
+                df[col] = le.fit_transform(df[col])
+            logging.info("Encoding complete")
+
+        df.to_csv("data/processed_data.csv", index=False)
+        logging.info("Preprocessed csv file has been saved into data/processed_data.csv")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+
     print('#' * 100)
 
 
--- data_splits/data_split.py
+++ data_splits/data_split.py
@@ -4,32 +4,39 @@ def data_spltting() -> None:
     from sklearn.model_selection import train_test_split
     import os
     import logging
-    logging.basicConfig(level = logging.INFO)
 
+    logging.basicConfig(level=logging.INFO)
     logging.info("Started Data Splitting Component")
-    
-    processed_data = pd.read_csv("data/processed_data.csv")
-    logging.info("Reading data from data/processed_data.csv")
-    target_column = 'Frequency of Purchases'
-    X = processed_data.loc[:, processed_data.columns != target_column]
-    y = processed_data.loc[:, processed_data.columns == target_column]
-    directory = 'data/splits'
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        logging.info(f"Directory '{directory}' created successfully!")
-    else:
-        logging.info(f"Directory '{directory}' already exists. Skipping creation")    
-        logging.warn(f"This behaviour is due to cache, make sure this is what you intended")    
+    try:
+        processed_data = pd.read_csv("data/processed_data.csv")
+        logging.info("Reading data from data/processed_data.csv")
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,stratify = y, random_state=1234)
-        
-    np.save(f'data/splits/X_train.npy', X_train)
-    np.save(f'data/splits/X_test.npy', X_test)
-    np.save(f'data/splits/y_train.npy', y_train)
-    np.save(f'data/splits/y_test.npy', y_test)
-    logging.info("Splits have been saved into the `data/splits/` directory")
-    print('#' * 100)
-    
+        target_column = 'Frequency of Purchases'
+        X = processed_data.loc[:, processed_data.columns != target_column]
+        y = processed_data.loc[:, processed_data.columns == target_column]
+
+        directory = 'data/splits'
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            logging.info(f"Directory '{directory}' created successfully!")
+        else:
+            logging.info(f"Directory '{directory}' already exists. Skipping creation")
+            logging.warning("This behavior is due to cache; ensure this is intended")
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=1234)
+
+        np.save(f'data/splits/X_train.npy', X_train)
+        np.save(f'data/splits/X_test.npy', X_test)
+        np.save(f'data/splits/y_train.npy', y_train)
+        np.save(f'data/splits/y_test.npy', y_test)
+
+        logging.info("Splits have been saved into the `data/splits/` directory")
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+    finally:
+        print('#' * 100)
+
 if __name__ == "__main__":
     data_spltting()
