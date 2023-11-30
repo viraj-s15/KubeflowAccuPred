3f63df077ca9821d68023dca7fd3330a24b61915
Viraj Shah <viraj.v.shah03@gmail.com>
2023-11-30 14:17:09 +0530

    feat: updated pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,31 +1,22 @@
 import kfp 
 from kfp import dsl
-from components import step_data_preprocessing,step_data_splitting,step_model_testing,step_hyperparam_optimisation
+from components import step_data_preprocessing,step_data_splitting,step_model_testing,step_hyperparam_optim
 
 @dsl.pipeline(
     name='Customer Frequency Training Pipeline',
-    description=''
+    description='A kubernetes pipeline for training a customer freq prediction model'
 )
-def customer_freq_training_pipeline(data_path: str):
-    vop = dsl.VolumeOp(
-        name="t-vol",
-        resource_name="t-vol", 
-        size="1Gi", 
-        modes=dsl.VOLUME_MODE_RWO
-    )
-    
-    data_preprocessing_task = step_data_preprocessing().add_pvolumes({data_path: vop.volume})
-    data_splitting_task = step_data_splitting().add_pvolumes({data_path: vop.volume}).after(data_preprocessing_task)
-    hyperparam_optim_task = step_hyperparam_optimisation().add_pvolumes({data_path: vop.volume}).after(data_splitting_task)
-    model_testing_task = step_model_testing().add_pvolumes({data_path: vop.volume}).after(hyperparam_optim_task)
+def customer_freq_training_pipeline():
+    data_preprocessing_task = step_data_preprocessing()
+    data_splitting_task = step_data_splitting().after(data_preprocessing_task)
+    hyperparam_optim_task = step_hyperparam_optim().after(data_splitting_task)
+    model_testing_task = step_model_testing().after(hyperparam_optim_task)
     
     data_preprocessing_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     data_splitting_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     hyperparam_optim_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     model_testing_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
 
-    pipeline_func = customer_freq_training_pipeline
-
     kfp.compiler.Compiler().compile(
         pipeline_func=customer_freq_training_pipeline,
-        package_path='Customer_freq_training.yaml')
+        package_path='compiled_pipelines/Customer_freq_training.yaml')
--- train_pipeline_execute.py
+++ train_pipeline_execute.py
@@ -1,22 +1,17 @@
 from kfp import Client
-from pipelines import customer_freq_training_pipeline
 import datetime
 
 client = Client()
 
 DATA_PATH = 'data/'
 
-pipeline_func = customer_freq_training_pipeline
 
 experiment_name = 'customer_freq_prediction' + str(datetime.datetime.now().date())
-run_name = pipeline_func.__name__ + ' run'
+run_name = 'customer_freq_prediction' + ' run'
 
-arguments = {"data_path": DATA_PATH}
-
-run_result = client.create_run_from_pipeline_func(
-    pipeline_func=pipeline_func,
+run_result = client.create_run_from_pipeline_package(
+    pipeline_func='compiled_pipelines/Customer_freq_training.yaml',
     experiment_name=experiment_name,
     run_name=run_name,
     namespace="kubeflow",
-    arguments=arguments
 )
