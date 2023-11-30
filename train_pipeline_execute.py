405d34cd53bc159062685e26b338c0166c10a91e
Viraj Shah <viraj.v.shah03@gmail.com>
2023-11-30 17:45:33 +0530

    feat: updated pipeline execution script

--- train_pipeline_execute.py
+++ train_pipeline_execute.py
@@ -1,18 +1,25 @@
+import argparse
 from kfp import Client
 import datetime
 
-client = Client()
+def run_pipeline(host):
+    client = Client(host=host)
+    DATA_PATH = 'data/'
 
-DATA_PATH = 'data/'
+    experiment_name = 'customer_freq_prediction' + str(datetime.datetime.now().date())
+    run_name = 'customer_freq_prediction' + ' run'
 
+    run_result = client.create_run_from_pipeline_package(
+        pipeline_func='compiled_pipelines/Customer_freq_training.yaml',
+        experiment_name=experiment_name,
+        run_name=run_name,
+        namespace="kubeflow",
+        enable_caching=False
+    )
 
-experiment_name = 'customer_freq_prediction' + str(datetime.datetime.now().date())
-run_name = 'customer_freq_prediction' + ' run'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run KFP pipeline")
+    parser.add_argument("--host", type=str, required=True, help="Kubeflow Pipelines host URL")
+    args = parser.parse_args()
 
-run_result = client.create_run_from_pipeline_package(
-    pipeline_func='compiled_pipelines/Customer_freq_training.yaml',
-    experiment_name=experiment_name,
-    run_name=run_name,
-    namespace="kubeflow",
-    enable_caching = False
-)
+    run_pipeline(args.host)
